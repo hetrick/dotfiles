50a88f3b26ffa76d90a72394666e0ad1bdee7538
noah hetrick <noah.hetrick@onxmaps.com>
2022-07-14 22:51:16 -0400

    postpone vs code settings for now...

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,7 +44,7 @@ ln -s $HOME/.dotfiles/.vscode $HOME/.vscode
 # source .python
 
 pretty_print "setting up vs code..."
-ln -s $HOME/.dotfiles/.vscode-settings.json $HOME/Library/Application Support/Code/User/settings.json
+# ln -s $HOME/.dotfiles/.vscode-settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 source .vscode-extensions
 
 pretty_print "setting up dock..."
