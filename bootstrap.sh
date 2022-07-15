0484bc7ac094479f5926dd35b6b4e695368b1060
noah hetrick <noahhetrick@gmail.com>
2022-07-15 10:42:43 -0400

    bootstrap gitconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,7 +34,7 @@ rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # remove (if necessary) and symlink .p10k.zsh from .dotfiles
-rm -rf $HOME/./.p10k.zsh
+rm -rf $HOME/.p10k.zsh
 ln -s $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
 
 pretty_print "setting up python environment..."
@@ -43,6 +43,10 @@ source .python
 pretty_print "setting up vs code..."
 ln -s $HOME/.dotfiles/.vscode-settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 source .vscode-extensions
+
+pretty_print "setting up gitconfig..."
+rm -rf $HOME/.gitconfig
+ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 
 ### disabled until dockutil supports OSX Monterey
 # pretty_print "setting up dock..."
